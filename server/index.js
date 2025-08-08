25c2be09e6f2c0ad0b687a0435b519fa8c3a9305
alisattar081 <52017194+alisattar081@users.noreply.github.com>
2025-08-08 02:56:39 -0400

    Add CORS middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // serve static files from the project root
 app.use(express.static(path.join(__dirname, '..')));
