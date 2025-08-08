fb926f18170f94db069af0faf1185547b215d77a
alisattar081 <52017194+alisattar081@users.noreply.github.com>
2025-08-08 02:28:55 -0400

    Add mobile navigation toggle

---
+++ nav.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('nav');
+if (navToggle && nav) {
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+}
