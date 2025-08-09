12623ab4fe3d501250242db011da7274e4a6d9ca
alisattar081 <52017194+alisattar081@users.noreply.github.com>
2025-08-09 15:59:00 -0400

    Use EJS templates with shared header and footer

--- server/index.js
+++ server/index.js
@@ -7,10 +7,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Set up EJS templating
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // serve static files from the project root
 app.use(express.static(path.join(__dirname, '..')));
 
 app.use('/users', usersRouter);
+
+// Render EJS pages for each HTML route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/:page.html', (req, res) => {
+  res.render(req.params.page);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
